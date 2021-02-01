45f4593bc88c408232cc33a0816b1b6cc5e636c6
liy <liy@qq.com>
2021-02-01 17:51:35 +0800

    e

---
+++ common/config.js
@@ -0,0 +1,8 @@
+// const ROOTPATH = "http://39.105.40.121/app-store-api";
+const ROOTPATH = "https://test.jiexiaohei.com/app-store-api";
+const ROOTPATH1 = "https://www.gomyorder.cn/tao";
+module.exports = {
+    APIHOST: ROOTPATH,
+    APIHOST1: ROOTPATH1,
+    ROOTPATH: ROOTPATH
+};
